a6d4ff9b9c220dfa70648ee67cb1474eb7257ace
Hironsan <light.tree.1.13@gmail.com>
2017-07-21 15:53:12 +0900

    add config file

---
+++ anago/config.py
@@ -0,0 +1,12 @@
+class Config(object):
+
+    learning_rate = 1.0
+    max_grad_norm = 5
+    num_layers = 1
+    num_steps = 50
+    hidden_size = 200
+    max_epoch = 5
+    dropout = 0.5
+    lr_decay = 0.5
+    batch_size = 64
+    vocab_size = 10000
