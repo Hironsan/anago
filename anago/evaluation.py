daaaa3665f6596cf2ac7841f78cd867bb101f928
h-nakayama <light.tree.1.13@gmail.com>
2017-08-22 06:02:16 +0900

    add evaluator boilerplate

--- anago/evaluation.py
+++ anago/evaluation.py
@@ -1,7 +1,20 @@
+from anago.data.reader import batch_iter, load_word_embeddings
+from anago.models.models import SeqLabeling
+
+
 class Evaluator(object):
 
-    def __init__(self, *params):
-        pass
+    def __init__(self, config):
+        self.config = config
 
     def eval(self, x_test, y_test):
-        pass
+        p = None  # load preprocessor
+        train_steps, train_batches = batch_iter(
+            list(zip(x_test, y_test)), self.config.batch_size, preprocessor=p)
+
+        embeddings = load_word_embeddings(p.vocab_word, self.config.glove_path, self.config.word_dim)
+        self.config.char_vocab_size = len(p.vocab_char)
+
+        model = SeqLabeling(self.config, embeddings, len(p.vocab_tag))
+        model.load()  # load weights
+        model.evaluate_generator(train_batches, train_steps)
