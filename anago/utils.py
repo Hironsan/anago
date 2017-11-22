acad40acea1036ce11f139f2d8a83b01891c77a9
h-nakayama <light.tree.1.13@gmail.com>
2017-11-22 21:43:21 +0900

    Implement download method

--- anago/utils.py
+++ anago/utils.py
@@ -1,8 +1,21 @@
+import io
+import os
+import zipfile
+
+import requests
+
+
 def download(url, save_dir='.'):
-    """Downloads trained weights and config.
+    """Downloads trained weights, config and preprocessor.
 
     Args:
         url (str): target url.
         save_dir (str): store directory.
     """
-    pass
+    print('Downloading...')
+    r = requests.get(url, stream=True)
+    with zipfile.ZipFile(io.BytesIO(r.content)) as f:
+        if not os.path.exists(save_dir):
+            os.mkdir(save_dir)
+        f.extractall(save_dir)
+    print('Complete!')
