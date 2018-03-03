8c74a47981988dd251c8fef1516bb034e521d3ad
Hironsan <hiroki.nakayama.py@gmail.com>
2018-03-03 21:34:50 +0900

    Update utils

--- anago/utils.py
+++ anago/utils.py
@@ -1,12 +1,16 @@
+"""
+Utility functions.
+"""
 import io
 import os
 import zipfile
 
+import numpy as np
 import requests
 
 
 def download(url, save_dir='.'):
-    """Downloads trained weights, config and preprocessor.
+    """Download a trained weights, config and preprocessor.
 
     Args:
         url (str): target url.
@@ -19,3 +23,80 @@ def download(url, save_dir='.'):
             os.mkdir(save_dir)
         f.extractall(save_dir)
     print('Complete!')
+
+
+def load_data_and_labels(filename):
+    """Loads data and label from a file.
+
+    Args:
+        filename (str): path to the file.
+
+        The file format is tab-separated values.
+        A blank line is required at the end of a sentence.
+
+        For example:
+        ```
+        EU	B-ORG
+        rejects	O
+        German	B-MISC
+        call	O
+        to	O
+        boycott	O
+        British	B-MISC
+        lamb	O
+        .	O
+
+        Peter	B-PER
+        Blackburn	I-PER
+        ...
+        ```
+
+    Returns:
+        tuple(numpy array, numpy array): data and labels.
+
+    Example:
+        >>> filename = 'conll2003/en/ner/train.txt'
+        >>> data, labels = load_data_and_labels(filename)
+    """
+    sents, labels = [], []
+    with open(filename) as f:
+        words, tags = [], []
+        for line in f:
+            line = line.rstrip()
+            if len(line) == 0 or line.startswith('-DOCSTART-'):
+                if len(words) != 0:
+                    sents.append(words)
+                    labels.append(tags)
+                    words, tags = [], []
+            else:
+                word, tag = line.split('\t')
+                words.append(word)
+                tags.append(tag)
+    return np.asarray(sents), np.asarray(labels)
+
+
+def batch_iter(data, labels, batch_size, shuffle=True, preprocessor=None):
+    num_batches_per_epoch = int((len(data) - 1) / batch_size) + 1
+
+    def data_generator():
+        """
+        Generates a batch iterator for a dataset.
+        """
+        data_size = len(data)
+        while True:
+            # Shuffle the data at each epoch
+            if shuffle:
+                shuffle_indices = np.random.permutation(np.arange(data_size))
+                shuffled_data = data[shuffle_indices]
+                shuffled_labels = labels[shuffle_indices]
+            else:
+                shuffled_data = data
+                shuffled_labels = labels
+
+            for batch_num in range(num_batches_per_epoch):
+                start_index = batch_num * batch_size
+                end_index = min((batch_num + 1) * batch_size, data_size)
+                X, y = shuffled_data[start_index: end_index], shuffled_labels[start_index: end_index]
+                yield preprocessor.transform(X, y)
+
+    return num_batches_per_epoch, data_generator()
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,12 @@
 import os
 import unittest
 
-from anago.reader import load_data_and_labels, batch_iter
+from anago.utils import load_data_and_labels, batch_iter
 
 from anago.preprocess import DynamicPreprocessor, StaticPreprocessor
 
 
-class ReaderTest(unittest.TestCase):
+class TestUtils(unittest.TestCase):
 
     def setUp(self):
         self.filename = os.path.join(os.path.dirname(__file__), '../data/conll2003/en/ner/test.txt')
