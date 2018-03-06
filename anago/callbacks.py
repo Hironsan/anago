c99112162665aaf2667d1810d57982d0681f18e5
Hironsan <hiroki.nakayama.py@gmail.com>
2018-03-06 23:25:50 +0900

    Update F1 callback

--- anago/callbacks.py
+++ anago/callbacks.py
@@ -58,14 +58,12 @@ class F1score(Callback):
     def on_epoch_end(self, epoch, logs={}):
         label_true = []
         label_pred = []
-        for i, (data, label) in enumerate(self.valid_batches):
-            if i == self.valid_steps:
-                break
-            y_true = label
+        for i in range(self.valid_steps):
+            x_true, y_true = next(self.valid_batches)
             y_true = np.argmax(y_true, -1)
-            sequence_lengths = data[-1]  # shape of (batch_size, 1)
+            sequence_lengths = x_true[-1]  # shape of (batch_size, 1)
             sequence_lengths = np.reshape(sequence_lengths, (-1,))
-            y_pred = self.model.predict_on_batch(data)
+            y_pred = self.model.predict_on_batch(x_true)
             y_pred = np.argmax(y_pred, -1)
 
             y_true = self.p(y_true)
