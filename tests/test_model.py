d7b776c521fa13d74ee455c6ea56fc431f5dc321
Hironsan <hiroki.nakayama.py@gmail.com>
2018-03-07 07:06:15 +0900

    Update test model

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,12 +1,9 @@
 import os
 import unittest
 
-from anago.utils import load_data_and_labels
 from anago.models import BiLSTMCRF
 
-get_path = lambda path: os.path.join(os.path.dirname(__file__), path)
-DATA_ROOT = get_path('../data/conll2003/en/ner')
-SAVE_ROOT = get_path('models')  # trained model
+SAVE_ROOT = os.path.join(os.path.dirname(__file__), 'models')
 
 
 class TrainerTest(unittest.TestCase):
@@ -18,18 +15,11 @@ class TrainerTest(unittest.TestCase):
         cls.weights_file = os.path.join(SAVE_ROOT, 'weights.h5')
         cls.params_file = os.path.join(SAVE_ROOT, 'params.json')
 
-    def setUp(self):
-        data_path = os.path.join(DATA_ROOT, 'train.txt')
-        self.X, self.y = load_data_and_labels(data_path)
-
     def test_build_model(self):
         model = BiLSTMCRF(char_vocab_size=100,
                           word_vocab_size=10000,
                           ntags=10)
         model.build_model()
-
-    def test_predict(self):
-        pass
 
     def test_save(self):
         model = BiLSTMCRF(char_vocab_size=100,
