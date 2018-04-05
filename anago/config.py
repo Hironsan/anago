dab4ffa1c17d59f0366e442122662d5fa06062e2
ismael <ismaelfm_@ciencias.unam.mx>
2018-04-05 18:37:18 -0500

    Deleting unused params and attrs of training config #45

--- anago/config.py
+++ anago/config.py
@@ -43,9 +43,13 @@ class ModelConfig(object):
 class TrainingConfig(object):
     """Wrapper class for training hyperparameters."""
 
-    def __init__(self, batch_size=20, optimizer='adam', learning_rate=0.001, lr_decay=0.9,
-                 clip_gradients=5.0, max_epoch=15, early_stopping=True, patience=3,
-                 train_embeddings=True, max_checkpoints_to_keep=5):
+    def __init__(self,
+                 batch_size=20,
+                 optimizer='adam',
+                 max_epoch=15,
+                 early_stopping=True,
+                 patience=3,
+                 max_checkpoints_to_keep=5):
 
         # Batch size
         self.batch_size = batch_size
@@ -53,22 +57,12 @@ class TrainingConfig(object):
         # Optimizer for training the model.
         self.optimizer = optimizer
 
-        # Learning rate for the initial phase of training.
-        self.learning_rate = learning_rate
-        self.lr_decay = lr_decay
-
-        # If not None, clip gradients to this value.
-        self.clip_gradients = clip_gradients
-
         # The number of max epoch size
         self.max_epoch = max_epoch
 
         # Parameters for early stopping
         self.early_stopping = early_stopping
         self.patience = patience
-
-        # Fine-tune word embeddings
-        self.train_embeddings = train_embeddings
 
         # How many model checkpoints to keep.
         self.max_checkpoints_to_keep = max_checkpoints_to_keep
