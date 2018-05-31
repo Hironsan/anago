3effd00a12613cdee9aa89bdfee2c294efc5b6c9
Hironsan <hiroki.nakayama.py@gmail.com>
2018-05-31 18:10:40 +0900

    Remove get_callbacks

--- anago/callbacks.py
+++ anago/callbacks.py
@@ -1,50 +1,9 @@
 """
 Custom callbacks.
 """
-import os
-
 import numpy as np
-from keras.callbacks import Callback, TensorBoard, EarlyStopping, ModelCheckpoint
+from keras.callbacks import Callback
 from seqeval.metrics import f1_score
-
-
-def get_callbacks(log_dir=None, valid=(), checkpoint_dir=None, early_stopping=True):
-    """Get callbacks.
-
-    Args:
-        log_dir (str): the destination to save logs(for TensorBoard).
-        valid (tuple): data for validation.
-        checkpoint_dir (bool): Whether to use checkpoint.
-        early_stopping (bool): whether to use early stopping.
-
-    Returns:
-        list: list of callbacks
-    """
-    callbacks = []
-
-    if log_dir:
-        if not os.path.exists(log_dir):
-            os.mkdir(log_dir)
-            print('Successfully made a directory: {}'.format(log_dir))
-        callbacks.append(TensorBoard(log_dir))
-
-    if valid:
-        callbacks.append(F1score(*valid))
-
-    if checkpoint_dir:
-        if not os.path.exists(checkpoint_dir):
-            os.mkdir(checkpoint_dir)
-            print('Successfully made a directory: {}'.format(checkpoint_dir))
-
-        file_name = '_'.join(['model_weights', '{epoch:02d}', '{f1:2.2f}']) + '.h5'
-        save_callback = ModelCheckpoint(os.path.join(checkpoint_dir, file_name),
-                                        monitor='f1', save_weights_only=True)
-        callbacks.append(save_callback)
-
-    if early_stopping:
-        callbacks.append(EarlyStopping(monitor='f1', patience=3, mode='max'))
-
-    return callbacks
 
 
 class F1score(Callback):
