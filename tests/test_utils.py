31200e0a5437878c35fad42743758cc9cda85d56
Hironsan <hiroki.nakayama.py@gmail.com>
2018-06-01 09:05:27 +0900

    Update test_utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,6 +2,7 @@ import os
 import unittest
 
 from anago.utils import load_data_and_labels, batch_iter, Vocabulary
+from anago.preprocessing import IndexTransformer
 
 
 class TestUtils(unittest.TestCase):
@@ -10,14 +11,15 @@ class TestUtils(unittest.TestCase):
         self.filename = os.path.join(os.path.dirname(__file__), '../data/conll2003/en/ner/test.txt')
 
     def test_extract(self):
-        sents, labels = load_data_and_labels(self.filename)
-        self.assertTrue(len(sents) == len(labels))
+        X, y = load_data_and_labels(self.filename)
+        self.assertTrue(len(X) == len(y))
 
     def test_batch_iter(self):
-        sents, labels = load_data_and_labels(self.filename)
+        X, y = load_data_and_labels(self.filename)
         batch_size = 32
-        p = DynamicPreprocessor()
-        steps, batches = batch_iter(list(zip(sents, labels)), batch_size, preprocessor=p)
+        p = IndexTransformer()
+        p.fit(X, y)
+        steps, batches = batch_iter(X, y, batch_size, preprocessor=p)
         self.assertEqual(len([_ for _ in batches]), steps)  # Todo: infinite loop
 
 
